4dfc6b22b6c84fa38cde823cc6d636f25f6f7757
Daniel Stutzman <dtstutz@gmail.com>
2017-11-30 14:43:16 -0700

    Configure provider with username and password

--- provider.go
+++ provider.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"github.com/danielstutzman/go-belugacdn"
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
 func Provider() *schema.Provider {
 	return &schema.Provider{
-		ResourcesMap: map[string]*schema.Resource{},
+		Schema: map[string]*schema.Schema{
+			"username": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("BELUGACDN_USERNAME", nil),
+				Description: "BelugaCDN username",
+			},
+			"password": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("BELUGACDN_PASSWORD", nil),
+				Description: "BelugaCDN password",
+			},
+		},
+		ResourcesMap:  map[string]*schema.Resource{},
+		ConfigureFunc: configureProvider,
 	}
+}
+
+func configureProvider(d *schema.ResourceData) (interface{}, error) {
+	username := d.Get("username").(string)
+	password := d.Get("password").(string)
+
+	return &belugacdn.Config{
+		Username: username,
+		Password: password,
+	}, nil
 }
